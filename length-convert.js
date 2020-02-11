b1fdfa9b5d4b7e63346356731fc197304e325dd4
Naqsh Thind <naqsht@sfu.ca>
2020-02-11 14:19:10 -0800

    Unit-COnverter first commit

---
+++ length-convert.js
@@ -0,0 +1,6 @@
+var valNum = document.getElementById('length_Value');
+var length_from = document.getElementById('length-from').value;
+var length_to = document.getElementById("length-to");
+
+console.log(valNum);
+
